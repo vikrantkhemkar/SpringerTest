9842d68d65e5c087085207ec3dfdcf100774f0da
vikrant <vikrant.khemkar.ext@bbc.co.uk>
2019-01-31 14:08:30 +0000

    WIP- User journey feature and step definitions (#1)

--- features/step_definitions/Search.js
+++ features/step_definitions/Search.js
@@ -1,8 +1,20 @@
-const { Then, When, Given } = require('cucumber');
-const browser = require('../support/SeleniumSetup');
-const assert = require('assert');
 const config = require('../helpers/config');
+const pageObjects = require('../helpers/pageObjects');
 
 When(/^I launch Springer home page$/, async function(){
    await this.browser.get(config.baseUrl);
+});
+When(/^I search with "(.*)" in search text box$/, async function(searchTerm){
+    await pageObjects.searchOperation(searchTerm);
+});
+
+Then(/^I can see the search result$/, async function(){
+    const result = await pageObjects.verifySearchResult();
+    console.log(result);
+});
+When(/^I open the first record from result$/, async function(){
+    await pageObjects.openArticle();
+});
+Then(/^I see the details of that article$/, async function(){
+    const details= await pageObjects.verifyArticleDetails();
 });
